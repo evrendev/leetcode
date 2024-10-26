cae5bcb4c31dfdb63f92d3ed057d45e949457add
Evren Yeniev <mail@evren.dev>
2024-10-26 12:01:31 +0200

    Refactor deleteMiddle function to optimize deletion of middle node in linked list

--- src/leetcode-75/linked-list/2095-delete-the-middle-node-of-linked-list.js
+++ src/leetcode-75/linked-list/2095-delete-the-middle-node-of-linked-list.js
@@ -10,21 +10,18 @@
  * @return {ListNode}
  */
 const deleteMiddle = (head) => {
-  // Liste boşsa veya sadece bir eleman varsa, başı döndür
   if (!head || !head.next) return null;
 
   let slowPointer = head;
   let fastPointer = head;
   let previousNode = null;
 
-  // fastPointer her iki adımda bir hareket ederken, slowPointer her adımda bir ilerliyor
   while (fastPointer && fastPointer.next) {
     previousNode = slowPointer;
     slowPointer = slowPointer.next;
     fastPointer = fastPointer.next.next;
   }
 
-  // Middle node'u atla, yani ortadaki elemanı sil
   previousNode.next = slowPointer.next;
 
   return head;
